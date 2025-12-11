d564ff7af743c14f2ac57002adae789f504c4b87
dugget <twitt23@gmail.com>
2025-12-10 20:43:31 -0500

    feat: Implement initial blog application structure with Express, EJS templates, and basic routing for posts.

--- capstone3/index.js
+++ capstone3/index.js
@@ -3,13 +3,18 @@ import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
-
 app.use(express.static("public"));
+
+app.use("/assets", express.static("assets"));
 
 const __filename = fileURLToPath(import.meta.url);
 
 app.get('/', (req, res) => {
     res.render("index.ejs");
+})
+
+app.get('/create', (req, res) => {
+    res.render("create.ejs");
 })
 
 app.listen(port, () => {
